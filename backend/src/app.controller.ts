bf5f26cba35d0de7889e631f6be84356c8f7fc0a
Jan Gross <jan.gross@regiojet.cz>
2025-12-30 15:31:15 +0100

    databse seeding

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { SeedService } from './seed/seed.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly seedService: SeedService
+) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('seed')
+  async seedData(): Promise<string> {
+    await this.seedService.seed();
+    return 'Data byla úspěšně vygenerována!';
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,23 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BetsModule } from './bets/bets.module';
 import { ConfigModule } from '@nestjs/config';
+import { SeedService } from './seed/seed.service';
+// Musíš naimportovat Schéma a Class, aby je AppModule znal
+import { Bets, BetsSchema } from './bets/schemas/newBetSchema'; 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI ?? ""), BetsModule],
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? ""),
+    
+    // DŮLEŽITÉ 1: Aby SeedService mohla pracovat s databází v rámci AppModule,
+    // musíme sem zaregistrovat schéma (stejně jako jsi to dělal v BetsModule).
+    MongooseModule.forFeature([{ name: Bets.name, schema: BetsSchema }]),
+    
+    BetsModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  // DŮLEŽITÉ 2: SeedService musí být v providers, jinak ji Controller nenajde
+  providers: [AppService, SeedService], 
 })
 export class AppModule { }
